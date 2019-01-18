a24c9e9fd13b3fa0717dbae9584d9279c672afd5
Alexander Quach <alexquac@gmail.com>
2019-01-17 18:55:12 -0800

    updates to integrate Travis CI and add tests

--- server.js
+++ server.js
@@ -18,6 +18,41 @@ app.use(express.json());
 
 app.use('/blog-posts', blogRouter);
 
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listneing on port ${port}`);
+        resolver(server);
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+};
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+};
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
+module.exports = {app, runServer, closeServer};
